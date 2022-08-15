a0196b6ad271e66d5884a84d43d9624c6d363007
Dong <donking0030@gmail.com>
2022-08-15 14:28:59 +0800

    fix constants

--- AutoAPITest/src/main/java/org/tcl/autotest/utils/Constants.java
+++ AutoAPITest/src/main/java/org/tcl/autotest/utils/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static final String SEND_EMAIL_NAME = "724847367";
     // 开启POP3/SMTP服务得到的授权码
     // -------------  注意 -------------该数据需保密
-    public static final String SEND_EMAIL_CODE = "tpchyhiwzwvxbchj";
+    public static final String SEND_EMAIL_CODE = "xxx";
 
     public static final String MAIL_FILE_NAME = "接口自动化测试报告.html";
 
